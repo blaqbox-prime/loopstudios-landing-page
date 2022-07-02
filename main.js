ee4953a0facf10ccc6488a41dd57630fcd73fcdc
Karabo Sambo <karabosambo@outlook.com>
2022-07-02 22:58:19 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function openDrawer(){
+    document.getElementById('mobile_nav').style.width = '100%';
+}
+
+function closeDrawer(){
+    document.getElementById('mobile_nav').style.width = '0';
+}
